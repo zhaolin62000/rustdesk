618bf1cdb04e7a258b54457313cb053fff4a54a9
zhaolin62000 <77498830+zhaolin62000@users.noreply.github.com>
2026-08-03 21:33:41 +0800

    Change default URL from Rustdesk to Gele

--- src/common.rs
+++ src/common.rs
@@ -1080,7 +1080,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://www.gele.eu.org:2114".to_owned()
 }
 
 #[inline]
